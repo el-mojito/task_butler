8addc6059c09f47381f30f0436f1b81b48c0cb4e
Johannes Peter <jopeter@gmx.de>
2026-05-05 10:34:46 +0200

    Update skeleton

--- custom_components/task_butler/const.py
+++ custom_components/task_butler/const.py
@@ -7,4 +7,5 @@ from homeassistant.util import dt as dt_util
 LOGGER: Logger = getLogger(__package__)
 
 DOMAIN = "task_butler"
-PLATFORMS = ["binary_sensor", "button"]
+
+PLATFORMS: list[str] = ["binary_sensor", "button"]
